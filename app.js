f6c3bc8ffb447138584c3acfeb5cf28e66eb16fb
kazucv <kazu.cv@gmail.com>
2025-12-29 14:16:19 +0900

    chore: bust cache for app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+console.log("APP VERSION: 2025-12-29 14:16 POST");
+document.getElementById("status").textContent =
+  "APP VERSION: 2025-12-29 14:16 POST";
+
 const GAS_URL =
   "https://script.google.com/macros/s/AKfycbx2e8Xd8kAQ--kWErdGY7CBtsJ8gDSD87SEQbtDHrfM5HL0xxGhfpzZ8hQ5Qjj8bRg/exec";
 
