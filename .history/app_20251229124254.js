5a495095334e04260d4b657317cf9e29fe023b53
kazucv <kazu.cv@gmail.com>
2026-01-06 17:32:07 +0900

    initial commit

--- .history/app_20251229124254.js
+++
@@ -1,7 +0,0 @@
-const GAS_URL =
-  "https://script.google.com/macros/s/AKfycbx2e8Xd8kAQ--kWErdGY7CBtsJ8gDSD87SEQbtDHrfM5HL0xxGhfpzZ8hQ5Qjj8bRg/exec";
-
-fetch(GAS_URL)
-  .then((r) => r.json())
-  .then((data) => console.log("GAS:", data))
-  .catch((err) => console.error("fetch error:", err));
